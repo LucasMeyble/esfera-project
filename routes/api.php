8c0cca716f4b980b0e4beda110a5004535d07647
LucasMeyble <lucasmeyble123@gmail.com>
2024-04-07 04:23:15 -0300

    feat: rotas de users e tasks

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,16 @@
 <?php
 
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+use App\Http\Controllers\UserController;
 
-Route::apiResource('/users', UserController::class);
-// Route::delete('/users/{id}', [UserController::class, 'destroy']);
-// Route::patch('/users/{id}', [UserController::class, 'update']);
-// Route::get('/users/{id}', [UserController::class, 'show']);
-// Route::get('/users', [UserController::class, 'index']);
-// Route::post('/users', [UserController::class, 'store']);
+
 
 Route::get('/', function () {
     return response()->json([
         'success' => true
     ]);
 });
+
+Route::resource('/tasks', TasksController::class);
+Route::resource('/users', UserController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/tasks', TasksController::class);
+Route::resource('/users', UserController::class);
