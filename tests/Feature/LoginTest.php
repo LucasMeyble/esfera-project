c8c6f89c72dcbf5cf45a546b931d7854750adb4a
LucasMeyble <lucasmeyble123@gmail.com>
2024-04-10 00:13:33 -0300

    fez: testes de login

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
@@ -23,19 +23,34 @@ class LoginTest extends TestCase
 
     public function test_user_can_login_with_correct_credentials()
     {
-
         $user = User::factory()->create(
             [
                 'password' => bcrypt('i-love-laravel'),
             ]
         );
-
-        $response = $this->post(route('auth.login'), [
+        $response = $this->post('login', [
             'email' => $user->email,
             'password' => 'i-love-laravel',
         ]);
 
         $response->assertRedirect('tasks');
         $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_can_login_with_incorrect_credentials()
+    {
+        $user = User::factory()->create(
+            [
+                'password' => bcrypt('i-love-laravel'),
+            ]
+        );
+        $response = $this->post('login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/');
+        $response->assertStatus(302);
+        $this->assertGuest();
     }
 }
